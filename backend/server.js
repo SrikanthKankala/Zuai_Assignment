d54d886f643a09408b5cf0aaa64905f2ddb37972
SrikanthKankala <kankalasrikanth1@gmail.com>
2024-08-24 14:11:28 +0530

    Zuai

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,10 @@
 const app = require('./app');
 
-// const PORT = process.env.PORT || 3000;
-
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-// });
-
-
-
-
-// C:\zuai-project\backend\server.js
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const postsRouter = require('./routes/posts'); // Your routes file
-
-const app = express();
-app.use(bodyParser.json());
-app.use(cors()); // Enable CORS to allow frontend requests
-
-// Base path '/api'
-app.use('/api/posts', postsRouter);
-
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
+
+
+
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://thousif-posts-backend.onrender.com/api',
+    baseURL: 'https://zuai-assignment-fgva.onrender.com/api',
+    
 });
 
 export const getPosts = () => api.get('/posts');
